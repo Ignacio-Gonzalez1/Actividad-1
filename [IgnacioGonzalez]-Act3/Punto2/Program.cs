5a80a380329717ec0740a228eb6611bbff375e77
Ignacio-Gonzalez1 <ignaciorubengonzalezfleitas@gmail.com>
2026-04-06 00:09:59 -0300

    Arregle el Error del punto 2 de la actividad 3

--- [IgnacioGonzalez]-Act3/Punto2/Program.cs
+++ [IgnacioGonzalez]-Act3/Punto2/Program.cs
@@ -11,7 +11,7 @@ namespace Punto2
         static void Main(string[] args)
         {
             //Se ingresan tres valores por teclado, si todos son iguales se imprime la suma del primero con el segundo y a este resultado se lo multiplica por el tercero.
-            int Num1, Num2, Num3, multiplicacion;
+            int Num1, Num2, Num3, multiplicacion, suma;
             string linea;
 
             Console.WriteLine("El Primer Numero: ");
@@ -29,7 +29,10 @@ namespace Punto2
             if (Num1==Num2)
             {
                 if (Num2==Num3) {
-                multiplicacion = (Num1 + Num2) * Num3;
+                    suma = Num1 + Num2;
+                    multiplicacion = suma * Num3;
+                    Console.WriteLine("El resultado de la suma de los primeros dos numeros es: ");
+                    Console.WriteLine(suma);
                     Console.WriteLine("El resultado de la multiplicacion es: ");
                     Console.WriteLine(multiplicacion);
                 }
