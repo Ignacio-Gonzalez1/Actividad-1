4922f51f50b6466515fe59a0320305882e6eac4d
Alumno <alumno@et26.local>
2026-04-10 08:48:26 -0300

    Actividad 7 terminada

--- [IgnacioGonzalez]-Act7/Punto2/Program.cs
+++ [IgnacioGonzalez]-Act7/Punto2/Program.cs
@@ -6,10 +6,55 @@ using System.Threading.Tasks;
 
 namespace Punto2
 {
-    internal class Program
+    internal class Programa
     {
+        /*  Realizar un programa que pida la carga de dos vectores numéricos enteros de 4
+            elementos. Obtener la suma de los dos vectores, dicho resultado guardarlo en un
+            tercer vector del mismo tamaño. Sumar componente a componente.
+        */
+        private int[] Vector1, Vector2, Vector3;
+
+
+        public void CagarLosDosVectores()
+        {
+            Vector1 = new int[4];
+            Vector2 = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Cargar los datos en Vector 1: ");
+                string linea;
+                linea = Console.ReadLine();
+                Vector1[i]=int.Parse(linea);
+
+                Console.WriteLine("Cargar los datos en Vector 2: ");
+                linea = Console.ReadLine();
+                Vector2[i] = int.Parse(linea);
+            }
+        }
+        public void SumarLosVectores()
+        {
+            Vector3 = new int[4];
+            for (int f=0; f < 4; f++)
+            {
+                Vector3[f] = Vector1[f] + Vector2[f]; 
+            }
+        }
+        public void MostrarElResultadoSuma()
+        {
+            Console.WriteLine("Los Datos de la Suma de ambos Vectores es: ");
+            for (int j = 0; j < 4; j++)
+            {
+                Console.WriteLine(Vector3[j]);
+            }
+        }
+
         static void Main(string[] args)
         {
+            Programa VectorOne=new Programa();
+            VectorOne.CagarLosDosVectores();
+            VectorOne.SumarLosVectores();
+            VectorOne.MostrarElResultadoSuma();
+            Console.ReadKey();
         }
     }
 }
--- [IgnacioGonzalez]-Act7/Punto4/Program.cs
+++ [IgnacioGonzalez]-Act7/Punto4/Program.cs
@@ -1,15 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Punto4
 {
-    internal class Program
+    internal class Ordenado
     {
+        /*  Cargar un vector de 10 elementos y verificar posteriormente si el mismo está
+            ordenado de menor a mayor y de mayor a menor.
+        */
+        private int[] vector1;
+        private int mayor, menor;
+
+        public void CargarLosElementos()
+        {
+            vector1 = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Datos del Vector: ");
+                string linea;
+                linea = Console.ReadLine();
+                vector1[i] = int.Parse(linea);
+            }
+        }
+        public void Imprimir()
+        {
+            Console.WriteLine("Los Datos del Vector es: ");
+            for (int g = 0;g < 10;g++)
+            {
+                Console.WriteLine(vector1[g]);
+            }
+        }
+        public void CompararDatosVectorMenorMayor()
+        {
+            menor = vector1[0];
+            for (int f = 1; f < 10; f++)
+            {
+                if (menor <= vector1[f])
+                {
+                    menor = vector1[f];
+                }
+                else
+                    if (menor > vector1[f])
+                    {
+                        Console.WriteLine("No esta ordenado de Menor a Mayor");
+                        break;
+
+                    }
+            }
+            if (menor == vector1[9])
+            {
+                Console.WriteLine("Si esta ordenado de Menor a Mayor");
+            }
+           
+        }
+
+        public void CompararDatosVectorMayorMenor()
+        {
+            mayor = vector1[0];
+            for (int j = 1; j < 10; j++)
+            {
+                if (mayor>= vector1[j])
+                {
+                    mayor = vector1[j];
+                }else
+                    if (mayor < vector1[j])
+                    {
+                        Console.WriteLine("No esta ordenado de Mayor a Menor");
+                        break;
+                        
+                    }
+                
+            }
+            if (mayor == vector1[9])
+            {
+                Console.WriteLine("Si esta ordenado de Mayor a Menor");
+            }
+        }
+
         static void Main(string[] args)
         {
+            Ordenado per = new Ordenado();
+            per.CargarLosElementos();
+            per.Imprimir();
+            per.CompararDatosVectorMenorMayor();
+            per.CompararDatosVectorMayorMenor();
+            Console.ReadKey();
         }
     }
 }
