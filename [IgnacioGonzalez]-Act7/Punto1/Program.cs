54d148dbf962c2ae921136e03b07c661627c541a
Ignacio-Gonzalez1 <ignaciorubengonzalezfleitas@gmail.com>
2026-04-10 04:01:52 -0300

    Una parte de la Actividad 7(solo hecho 1 y 3)

---
+++ [IgnacioGonzalez]-Act7/Punto1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Punto1
+{
+    internal class Vector8Elementos
+    {
+        /*  Desarrollar un programa que permita ingresar un vector de 8 elementos, e informe:
+            El valor acumulado de todos los elementos del vector.
+            El valor acumulado de los elementos del vector que sean mayores a 36.
+            Cantidad de valores mayores a 50.
+        */
+        private int[] Elemetos;
+        private int sumaTotal, ValorAcumuladoMayor36, cantidadValoresMayor50;
+        
+
+        public void Ingresar8Elementos()
+        {
+            Elemetos = new int[8];
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine("Ingresa un Valor: ");
+                String linea;
+                linea = Console.ReadLine();
+                Elemetos[i] = int.Parse(linea);
+            }
+        }
+        public void ValorAcumulado()
+        {
+            sumaTotal = 0;
+            ValorAcumuladoMayor36 = 0;
+            cantidadValoresMayor50 = 0;
+            for (int f = 0;f < 8;f++)
+            {
+                sumaTotal= sumaTotal + Elemetos[f];
+                if (Elemetos[f] > 36)
+                {
+                    ValorAcumuladoMayor36= ValorAcumuladoMayor36 + Elemetos[f];
+                    if(Elemetos[f] > 50)
+                    {
+                        cantidadValoresMayor50 = cantidadValoresMayor50 + 1;
+                    }
+                }
+            }
+        }
+        public void ImprimirTodo()
+        {
+            Console.WriteLine("El Valor Acumulado de Todos los Elementos metidos es: " + sumaTotal);
+            Console.WriteLine("El Valor Acumulado de Elementos Mayores a 36 es: " + ValorAcumuladoMayor36);
+            Console.WriteLine("La Cantidad de Elementos Mayores a 50 es: " + cantidadValoresMayor50);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Vector8Elementos Ele= new Vector8Elementos();
+            Ele.Ingresar8Elementos();
+            Ele.ValorAcumulado();
+            Ele.ImprimirTodo();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ [IgnacioGonzalez]-Act7/Punto3/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto3
+{
+    internal class Partidas
+    {
+        /*  Pedir al usuario que ingrese las puntuaciones obtenidas en 6 partidas seguidas de
+            un videojuego. El programa debe:
+             Mostrar la puntuación más alta y la más baja.
+             Calcular el promedio de puntuación.
+             Contar cuántas veces superó los 500 puntos.
+        */
+        private int[] PartidasJugadas;
+        private int Mejor, Menor, Suma, PartidasMayor500P;
+        private double Promedio;
+        public void IngresarPartidas()
+        {
+            PartidasJugadas = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("Ingresar Puntuaciones de Partidas");
+                String linea;
+                linea = Console.ReadLine();
+                PartidasJugadas[i]=int.Parse(linea);
+            }
+        }
+        public void PuntuacionAltaYBaja()
+        {
+            Mejor= PartidasJugadas[0];
+            Menor= PartidasJugadas[0];
+            for(int i = 1;i < 6;i++)
+            {
+                if (PartidasJugadas[i] > Mejor)
+                {
+                    Mejor = PartidasJugadas[i];
+                }
+                if (PartidasJugadas[i] < Menor)
+                {
+                    Menor = PartidasJugadas[i];
+                }
+            }
+        }
+        public void PromedioPuntuaciones()
+        {
+            Suma = 0;
+            Promedio = 0;
+            for(int f = 0;f < 6; f++)
+            {
+                Suma= Suma + PartidasJugadas[f];
+            }
+            Promedio = (double)Suma / 6;
+        }
+        public void CantidadVecesQueSupero500P()
+        {
+            PartidasMayor500P = 0;
+            for (int j = 0; j < 6; j++)
+            {
+                if (PartidasJugadas[j] > 500)
+                {
+                    PartidasMayor500P = PartidasMayor500P + 1;
+                }
+            }
+        }
+        public void ImprimirTodo()
+        {
+            Console.WriteLine("La Puntuacion Mas Alta es: " + Mejor);
+            Console.WriteLine("La Puntuacion Mas Baja es: " + Menor);
+            Console.WriteLine("El Promedio de las Puntuaciones es: " + Promedio);
+            Console.WriteLine("La Cantidad de Puntuacioes Mejores a 500 es: " + PartidasMayor500P);
+        }
+
+        static void Main(string[] args)
+        {
+            Partidas puntuaciones1 = new Partidas();
+            puntuaciones1.IngresarPartidas();
+            puntuaciones1.PuntuacionAltaYBaja();
+            puntuaciones1.PromedioPuntuaciones();
+            puntuaciones1.CantidadVecesQueSupero500P();
+            puntuaciones1.ImprimirTodo();
+            Console.ReadKey();
+        }
+    }
+}
