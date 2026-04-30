083c680f1a00144edbca508d2db4f9835c63cacc
Ignacio-Gonzalez1 <ignaciorubengonzalezfleitas@gmail.com>
2026-04-30 04:05:29 -0300

    Actividad 10 la primera hecha pero falta

---
+++ [IgnacioGonzalez]-Act10/Punto1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto1
+{
+    internal class Programa
+    {
+        /*  Crear una matriz de 2 filas y 5 columnas. Realizar la carga de
+            componentes por columna (es decir primero ingresar toda la primer
+            columna, luego la segunda columna y así sucesivamente)
+            Imprimir luego la matriz.
+        */
+
+        private int[,] Matriz;
+
+        public void Cargardatos()
+        {
+            Matriz = new int[2, 5];
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Fila: " +(i + 1));
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.WriteLine("Ingresar los datos: ");
+                    string linea;
+                    linea = Console.ReadLine();
+                    Matriz[i,j]=int.Parse(linea);
+                }
+            }
+        }
+        public void ImprimirTodo()
+        {
+            Console.WriteLine("La Matriz total es:");
+            for (int f = 0; f < 2; f++)
+            {
+                Console.WriteLine("Fila: " + (f + 1));
+                for (int g = 0; g < 5; g++)
+                {
+                    Console.WriteLine(Matriz[f,g] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Programa Per1 = new Programa();
+            Per1.Cargardatos();
+            Per1.ImprimirTodo();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ [IgnacioGonzalez]-Act10/Punto2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto2
+{
+    internal class Programa
+    {
+        /*  Crear una matriz de n * m filas (cargar n y m por teclado) Intercambiar la
+            primer fila con la segunda. Imprimir luego la matriz.
+        */
+        private int[,] matriz; 
+        private int m, n;
+
+        public void cargarMyN()
+        {
+            m = 0;
+            n= 0;
+            Console.WriteLine("Cargar cuantas Filas quieres crear: ");
+            string linea;
+            linea = Console.ReadLine();
+            n= int.Parse(linea);
+            Console.WriteLine("Cargar cuantas Columnas quieres crear: ");
+            linea = Console.ReadLine();
+            m = int.Parse(linea);
+
+        }
+        public void cargardatos()
+        {
+            matriz = new int[n, m];
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Fila: " + (i + 1));
+                for (int j = 0; j < m; j++)
+                {
+                    Console.WriteLine("Ingresar los datos: ");
+                    string linea;
+                    linea = Console.ReadLine();
+                    matriz[i, j] = int.Parse(linea);
+                }
+            }
+        }
+        public void cambiarDatos()
+        {
+            for (int g = 0; g < n; g++)
+            {
+                for(int h = 0; h < m; h++)
+                {
+                    int Aux;
+                    Aux = matriz[g+1, h];
+                    matriz[g + 1, h] = matriz[g, h];
+                    
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Programa per1= new Programa();
+            per1.cargarMyN();
+            per1.cargardatos();
+            Console.ReadKey();
+        }
+    }
+}
