6238e1bd02814b8075ab1a52fdec6a196a66dc2d
Alumno <alumno@et26.local>
2026-05-08 09:15:14 -0300

    Actividad 11 Terminada

--- [IgnacioGonzalez]-Act11/Punto2/Program.cs
+++ [IgnacioGonzalez]-Act11/Punto2/Program.cs
@@ -6,10 +6,58 @@ using System.Threading.Tasks;
 
 namespace Punto2
 {
-    internal class Program
+    internal class Programa
     {
+        /*  Confeccionar una clase para administrar una matriz irregular de 5 filas y 1 columna la
+            primer fila, 2 columnas la segunda fila y así sucesivamente hasta 5 columnas la última fila
+            (crearla sin la intervención del operador)
+            Realizar la carga por teclado e imprimir posteriormente.
+        */
+        private int[][] matriz;
+        public void cantidadColumnas()
+        {
+            matriz = new int[5][];
+            matriz[0] = new int[1];
+            matriz[1] = new int[2];
+            matriz[2] = new int[3];
+            matriz[3] = new int[4];
+            matriz[4] = new int[5];
+        }
+        public void CargarDatos()
+        {
+            for (int i = 0; i < matriz.Length; i++)
+            {
+                Console.WriteLine("Cagar datos de la fila: " + (i + 1));
+                for (int j = 0; j < matriz[i].Length; j++)
+                {
+                    Console.WriteLine("Poner datos: ");
+                    string linea;
+                    linea = Console.ReadLine();
+                    matriz[i][j]=int.Parse(linea);
+                }
+            }
+
+        }
+        public void ImprimirTodo()
+        {
+            Console.WriteLine("La Matriz es asi: ");
+            for (int i = 0;i < matriz.Length; i++)
+            {
+                Console.Write("Los datos de la Fila: " + i + "  es: ");
+                for (int j = 0;j < matriz[i].Length; j++)
+                {
+                    Console.Write(" " + matriz[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
         static void Main(string[] args)
         {
+            Programa matriz1= new Programa();
+            matriz1.cantidadColumnas();    
+            matriz1.CargarDatos();
+            matriz1.ImprimirTodo();
+            Console.ReadKey();
         }
     }
 }
---
+++ [IgnacioGonzalez]-Act11/Punto3/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Punto3
+{
+    internal class Administracion
+    {
+        /*  Confeccionar una clase para administrar los días que han faltado los 3 empleados de una
+            empresa.
+            Definir un vector de 3 elementos de tipo string para cargar los nombres y una matriz
+            irregular para cargar los días que han faltado cada empleado (cargar el número de día que
+            faltó)
+            Cada fila de la matriz representa los días de cada empleado.
+                a. Mostrar los empleados con la cantidad de inasistencias.
+                b. Cuál empleado faltó menos días.
+        */
+        private string[] NomnbreEmpleados;
+        private int[][] diasfaltados;
+        private int cantidadDiasFaltados;
+
+        public void cargarDatos()
+        {
+            NomnbreEmpleados = new string[3];
+            diasfaltados = new int[3][];
+            for (int i = 0; i < NomnbreEmpleados.Length; i++)
+            {
+                Console.WriteLine("Poner el nombre de los empleados: ");
+                NomnbreEmpleados[i]= Console.ReadLine();
+            }
+            for(int i = 0;i < diasfaltados.Length; i++)
+            {
+                Console.WriteLine("Poner la cantidad de dias que falto  " + NomnbreEmpleados[i] + "  en la empresa: ");
+                string linea;
+                linea = Console.ReadLine();
+                cantidadDiasFaltados= int.Parse(linea);
+                diasfaltados[i]= new int[cantidadDiasFaltados];
+            }
+            for (int i = 0; i < diasfaltados.Length; i++)
+            {
+                Console.WriteLine("poner los dias faltados del empleado "+ NomnbreEmpleados[i]+ "  en la empresa: ");
+                for (int j = 0; j < diasfaltados[i].Length; j++)
+                {
+                    Console.WriteLine("Poner datos: ");
+                    string linea;
+                    linea = Console.ReadLine();
+                    diasfaltados[i][j] = int.Parse(linea);
+                }
+            }
+        }
+        public void ImprimirInasistencias()
+        {
+            for (int i = 0; i < diasfaltados.Length; i++)
+            {
+                Console.Write("Los dias que falto el empleado: " + NomnbreEmpleados[i] + "  es: ");
+                for (int j = 0; j < diasfaltados[i].Length; j++)
+                {
+                    Console.Write(" " + diasfaltados[i][j] + " ");
+                }
+                Console.Write("  la cantidad de dias que falto en total serian: " + diasfaltados[i].Length);
+                Console.WriteLine();
+            }
+        }
+        public void MenosInasistencias()
+        {
+            int menos = diasfaltados[0].Length;
+            int j = 0;
+            for (int i = 0;i < diasfaltados.Length; i++)
+            {
+                if(menos> diasfaltados[i].Length)
+                {
+                    menos= diasfaltados[i].Length;
+                    j= i;
+                }
+            }
+            Console.WriteLine("El Empleado que menos falto fue: " + NomnbreEmpleados[j] + "  la cantidad de dias que falto fue: " + menos);
+        }
+
+        static void Main(string[] args)
+        {
+            Administracion per1 = new Administracion();
+            per1.cargarDatos();
+            per1.ImprimirInasistencias();
+            per1.MenosInasistencias();
+            Console.ReadKey();
+        }
+    }
+}
