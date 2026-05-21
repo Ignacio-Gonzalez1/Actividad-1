bdd726f1e1d9c10ad8c73be21da5a27bdcd192d2
alumno <alumno@et26.dominio>
2026-05-21 10:04:53 -0300

    me falta hacer el 3 y 4

---
+++ [IgnacioGonzalez]-Act13/Punto1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto1
+{
+    internal class Empleado
+    {
+        /*  Confeccionar una clase que represente un empleado. Definir como atributos su
+            nombre y su sueldo. En el constructor cargar los atributos y luego en otro
+            método imprimir sus datos y por último uno que imprima un mensaje si debe
+            pagar impuestos (si el sueldo supera a 3000)
+        */
+        private string nombre;
+        private int sueldo;
+        public Empleado()
+        {
+            Console.WriteLine("Poner El Nombre del Empleado: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Poner El Sueldo de "+nombre+": ");
+            string linea;
+            linea= Console.ReadLine();
+            sueldo= int.Parse(linea);
+        }
+        public void Imprimirdatos()
+        {
+            Console.WriteLine("El nombre del Empleado es: "+nombre);
+            Console.WriteLine("Su sueldo es: "+sueldo);
+        }
+        public void PagaImpuestos()
+        {
+            if (sueldo > 3000)
+            {
+                Console.WriteLine("Debe Pagar Impuestos " + nombre);
+            }
+            else
+            {
+                Console.WriteLine("No Debe Pagar Impuestos " + nombre);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Empleado empleado1= new Empleado();
+            empleado1.Imprimirdatos();
+            empleado1.PagaImpuestos();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ [IgnacioGonzalez]-Act13/Punto2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto2
+{
+    internal class Operaciones
+    {
+        /*  Implementar la clase operaciones. Se deben cargar dos valores enteros en el
+            constructor, calcular su suma, resta, multiplicación y división, cada una en un
+            método, imprimir dichos resultados.
+        */
+        private int m, n, suma, resta, multiplicacion;
+        private double division;
+        public Operaciones()
+        {
+            Console.WriteLine("Introdusca el Primer Numero: ");
+            string linea;
+            linea = Console.ReadLine();
+            m=int.Parse(linea);
+            Console.WriteLine("Introdusca el Segundo Numero: ");
+            linea = Console.ReadLine();
+            n = int.Parse(linea);
+
+        }
+        public void sumar()
+        {
+            suma = m + n;
+        }
+        public void restar()
+        {
+            resta = m - n;
+        }
+        public void multiplicar()
+        {
+            multiplicacion = m * n;
+        }
+        public void Dividir()
+        {
+            division = (double)m / n;
+        }
+        public void ImprimirTodo()
+        {
+            Console.WriteLine("Los numeros son: " + m +" "+ n);
+            Console.WriteLine("El resultado de la suma es: " +suma);
+            Console.WriteLine("El resultado de la resta es: " +resta);
+            Console.WriteLine("El resultado de la multiplicacion es: " +multiplicacion);
+            Console.WriteLine("El resultado de la division es: " + division);
+        }
+        static void Main(string[] args)
+        {
+            Operaciones per1 = new Operaciones();
+            per1.sumar();
+            per1.restar();
+            per1.multiplicar();
+            per1.Dividir();
+            per1.ImprimirTodo();
+            Console.ReadKey();
+        }
+    }
+}
