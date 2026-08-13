c923a7f9c0ee9e4318d973d87f35f4282478d092
Ignacio-Gonzalez1 <ignaciorubengonzalezfleitas@gmail.com>
2026-08-13 10:10:39 -0300

    Solo la actividad 7 terminada

---
+++ [IgnacioGonzalez]-Act24/Punto1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto1
+{
+    class Plato
+    {
+        private string nombrePlato;
+        private double precio;
+        public string NombrePlato
+        {
+            set { nombrePlato = value; }
+            get { return nombrePlato; }
+        }
+        public double Precio
+        {
+            set { precio = value; }
+            get { return precio; }
+        }
+        public Plato(string nom, double pre)
+        {
+            nombrePlato= nom;
+            precio = pre;
+        }
+    }
+    class GestionComandas
+    {
+        List<Plato> ListaPlatos= new List<Plato>();
+
+        public void AgregarPlato()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Poner el Nombre del Plato: ");
+                string nom = Console.ReadLine();
+                Console.WriteLine("Poner el Precio del Plato: ");
+                string linea= Console.ReadLine().ToLower();
+                double pre = double.Parse(linea);
+                Plato agregarPlato = new Plato(nom, pre);
+                ListaPlatos.Add(agregarPlato);
+            }
+        }
+        public void MostrarComanda()
+        {
+            Console.WriteLine("La Lista total de Platos: ");
+            for (int i = 0; i < ListaPlatos.Count; i++)
+            {
+                Console.WriteLine("El nombre del Plato es: "+ListaPlatos[i].NombrePlato + " El precio de ese plato es: " + ListaPlatos[i].Precio);
+            }
+            Console.WriteLine("La cantidad total de platos es: "+ListaPlatos.Count);
+        }
+        public void CalcularTotalMesa()
+        {
+            double totalprecios= 0;
+            Console.WriteLine("El Precio total a cobrar sumando todos los precios es: ");
+            foreach (Plato item in ListaPlatos)
+            {
+                totalprecios += item.Precio;
+            }
+            Console.Write(totalprecios+"$");
+            Console.WriteLine();
+        }
+        public void CancelarPlato()
+        {
+            Console.WriteLine("Poner Un nonbre de un plato para eliminarlo: ");
+            string linea = Console.ReadLine().ToLower();
+            Plato aux = null;
+            foreach (Plato item in ListaPlatos)
+            {
+                if (item.NombrePlato == linea)
+                {
+                    aux = item;
+                    break;
+                }
+            }
+            if (aux != null)
+            {
+                ListaPlatos.Remove(aux);
+                Console.WriteLine("Se confirma la Eliminacion del Plato: " + aux.NombrePlato);
+            }
+            if (aux == null)
+            {
+                Console.WriteLine("Ese Plato no se encuentra.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            GestionComandas gestion= new GestionComandas();
+            gestion.AgregarPlato();
+            gestion.MostrarComanda();
+            gestion.CalcularTotalMesa();
+            gestion.CancelarPlato();
+            Console.ReadKey();
+        }
+    }
+}
